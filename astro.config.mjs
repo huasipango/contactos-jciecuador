9fa1db1b15686880bda000efd947facefa05acb6
Anthony Cabrera <anthony.cabrera@minu.mx>
2025-01-02 14:45:15 +0100

    Update configuration and layout for JCI Ecuador site: - Added Netlify adapter to astro.config.mjs for deployment. - Updated package.json to include @astrojs/netlify dependency. - Enhanced SEO metadata in Layout.astro for better visibility. - Changed page title to "Directorio de Contactos" in index.astro. - Removed outdated comment in index.astro for clarity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://contactos.jciecuador.com',
   output: 'server',
+
   vite: {
     ssr: {
       noExternal: ['canvas-confetti']
     }
-  }
-}); 
+  },
+
+  adapter: netlify()
+});
