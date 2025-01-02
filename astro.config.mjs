3e7df8fbc2d13876b1938eccafa262b472a2f0ec
Anthony Cabrera <anthony.cabrera@minu.mx>
2025-01-02 19:35:45 +0100

    Update site URL in astro.config.mjs and clean up redirects in netlify.toml
    - Changed the site URL from 'https://contactos.jciecuador.com' to 'https://contactos-jciecuador.netlify.app' for deployment on Netlify.
    - Removed outdated authentication redirects from netlify.toml to streamline routing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://contactos.jciecuador.com',
+  site: 'https://contactos-jciecuador.netlify.app',
   output: 'server',
 
   vite: {
